1c2a6c31394599177868897afca536c63c35e8cc
Edbts <bustos@weone.com.br>
2017-06-01 15:34:16 -0300

    Advogado

---
+++ advogado/public_html/_js/default.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    
+                $("#slider4").responsiveSlides({
+                    auto: true,
+                    pager: false,
+                    nav: false,
+                    speed: 2000,
+                    namespace: "callbacks"
+
+                });
+
+
+            var userFeed = new Instafeed({
+                get: 'user',
+                userId: '254267992',
+                accessToken: 'YOUR_ACCESS_TOKEN'
+            });
+            userFeed.run();
+            
+            $(function () {
+                $('#formquest').form({
+                    fields: {
+
+                        nome: {
+                            identifier: 'nome',
+                            rules: [
+                                {
+
+                                    type: 'empty',
+                                    prompt: 'Por favor digite seu nome '
+                                }
+                            ]
+
+                        },
+                        email: {
+                            identifier: 'mail',
+                            rules: [
+                                {
+
+                                    type: 'email',
+                                    prompt: 'Por favor digite um e-mail valido '
+                                }
+                            ]
+                        },
+                        telefone: {
+                            identifier: 'fone',
+                            rules: [
+                                {
+
+                                    type: 'number',
+                                    prompt: 'Por favor digite seu telefone valido '
+                                }
+                            ]
+                        },
+                        onSuccess: function (e) {
+                            e.preventDefault();
+                            alert('enviando formulário');
+                        }
+                    }
+                });
+            });
+
+
+})
